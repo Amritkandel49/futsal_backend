d72506c9ec0c8935d8b72f3ade2d8f9fc679d2fe
Amritkandel49 <amritkandel062@gmail.com>
2024-08-10 19:14:46 +0545

    sdc

--- src/app.js
+++ src/app.js
@@ -1,40 +1,46 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; img-src 'self' https:; script-src 'self'; style-src 'self' https:;"
+  );
+  next();
+});
+
 const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type, Authorization',
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type, Authorization",
   credentials: true,
 };
 
-app.options('*', cors(corsOptions));
+app.options("*", cors(corsOptions));
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
 
 app.use(express.json());
-app.use(express.urlencoded({
-    extended : true
-}))
-app.use(express.static('public'));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
 
+import ownerRouter from "./routes/owner.route.js";
+import trufRouter from "./routes/turf.route.js";
+import userRouter from "./routes/user.route.js";
+import bookingRouter from "./routes/booking.route.js";
 
-import ownerRouter from './routes/owner.route.js';
-import trufRouter from './routes/turf.route.js';
-import userRouter from './routes/user.route.js';
-import bookingRouter from './routes/booking.route.js';
-
-
-
-app.use('/api/v1/owners', ownerRouter);
-app.use('/api/v1/turfs', trufRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/bookings', bookingRouter);
-
+app.use("/api/v1/owners", ownerRouter);
+app.use("/api/v1/turfs", trufRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/bookings", bookingRouter);
 
 export default app;
